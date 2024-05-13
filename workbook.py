281862880d2d5d0e0216d7f2d8ab265f23983332
Thomeeng <thomas.pelletier@ems-ing.com>
2024-05-13 11:37:47 -0400

    Fonction de puissance electrique consomme

--- workbook.py
+++ workbook.py
@@ -5,3 +5,13 @@
 ###
 import numpy as np
 
+def pelec(V,I,f=1):
+    ##
+    # Puissance electrique consomme
+    # Variables : V, Tension d'alimentation; I, Courant consomme; f, facteur d'efficacite (default = 1)
+    ##
+    p = V*I*f
+    return p
+
+
+
